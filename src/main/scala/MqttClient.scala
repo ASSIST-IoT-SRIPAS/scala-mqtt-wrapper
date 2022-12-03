90ffe7d904d7067429bbd09b6ed743e5bf693451
madpeh <48161600+madpeh@users.noreply.github.com>
2022-12-03 03:53:41 +0100

    Add MqttClient

--- src/main/scala/MqttClient.scala
+++ src/main/scala/MqttClient.scala
@@ -1,5 +1,40 @@
 package pl.waw.ibspan.scala_mqtt_wrapper
 
-class MqttClient {
+import akka.NotUsed
+import akka.actor.typed.ActorSystem
+import akka.stream.alpakka.mqtt.streaming.Command
+import akka.stream.alpakka.mqtt.streaming.Connect
+import akka.stream.alpakka.mqtt.streaming.ConnectFlags
+import akka.stream.alpakka.mqtt.streaming.Event
+import akka.stream.alpakka.mqtt.streaming.MqttCodec
+import akka.stream.alpakka.mqtt.streaming.MqttSessionSettings
+import akka.stream.alpakka.mqtt.streaming.scaladsl.ActorMqttClientSession
+import akka.stream.alpakka.mqtt.streaming.scaladsl.Mqtt
+import akka.stream.scaladsl.Flow
+import akka.stream.scaladsl.Tcp
+import akka.util.ByteString
 
+
+import scala.concurrent.Future
+
+class MqttClient(mqttSettings: MqttSettings)(implicit system: ActorSystem[_]) {
+  val settings: MqttSessionSettings = MqttSessionSettings()
+  val session: ActorMqttClientSession = ActorMqttClientSession(settings)
+  val tcpConnection: Flow[ByteString, ByteString, Future[Tcp.OutgoingConnection]] =
+    Tcp(system).outgoingConnection(mqttSettings.host, mqttSettings.port)
+  val sessionId: ByteString = ByteString(generateUuid.toString)
+  val flow: Flow[Command[Nothing], Either[MqttCodec.DecodeError, Event[Nothing]], NotUsed] =
+    Mqtt
+      .clientSessionFlow(session, sessionId)
+      .join(tcpConnection)
+  val clientId: String = generateUuid.toString
+  val connection: Command[Nothing] =
+    Command[Nothing](
+      Connect(
+        clientId,
+        extraConnectFlags = ConnectFlags.CleanSession,
+        username = mqttSettings.username.getOrElse(""),
+        password = mqttSettings.password.getOrElse("")
+      )
+    )
 }
