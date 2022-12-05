018da41b7772734bb25a32a7d2d39ce40026cb1f
madpeh <48161600+madpeh@users.noreply.github.com>
2022-12-05 14:04:12 +0100

    Add flow common to source and sink

--- src/main/scala/MqttClient.scala
+++ src/main/scala/MqttClient.scala
@@ -2,25 +2,31 @@ package pl.waw.ibspan.scala_mqtt_wrapper
 
 import akka.NotUsed
 import akka.actor.typed.ActorSystem
+import akka.stream.OverflowStrategy
 import akka.stream.alpakka.mqtt.streaming.Command
 import akka.stream.alpakka.mqtt.streaming.Connect
 import akka.stream.alpakka.mqtt.streaming.Event
 import akka.stream.alpakka.mqtt.streaming.MqttCodec
 import akka.stream.alpakka.mqtt.streaming.MqttSessionSettings
+import akka.stream.alpakka.mqtt.streaming.Subscribe
 import akka.stream.alpakka.mqtt.streaming.scaladsl.ActorMqttClientSession
 import akka.stream.alpakka.mqtt.streaming.scaladsl.Mqtt
 import akka.stream.scaladsl.Flow
+import akka.stream.scaladsl.Keep
+import akka.stream.scaladsl.Source
+import akka.stream.scaladsl.SourceQueueWithComplete
 import akka.stream.scaladsl.Tcp
 import akka.util.ByteString
+import com.typesafe.scalalogging.LazyLogging
 
 import scala.concurrent.Future
 
-class MqttClient(mqttSettings: MqttSettings)(implicit system: ActorSystem[_]) {
+class MqttClient(mqttSettings: MqttSettings)(implicit system: ActorSystem[_]) extends LazyLogging {
   val settings: MqttSessionSettings = MqttSessionSettings()
   val session: ActorMqttClientSession = ActorMqttClientSession(settings)
   val tcpConnection: Flow[ByteString, ByteString, Future[Tcp.OutgoingConnection]] =
     Tcp(system).outgoingConnection(mqttSettings.host, mqttSettings.port)
-  val flow: Flow[Command[Nothing], Either[MqttCodec.DecodeError, Event[Nothing]], NotUsed] =
+  val sessionFlow: Flow[Command[Nothing], Either[MqttCodec.DecodeError, Event[Nothing]], NotUsed] =
     Mqtt
       .clientSessionFlow(session, mqttSettings.sessionId)
       .join(tcpConnection)
@@ -33,4 +39,15 @@ class MqttClient(mqttSettings: MqttSettings)(implicit system: ActorSystem[_]) {
         password = mqttSettings.password
       )
     )
+  val subscribeCommands: List[Command[Nothing]] =
+    mqttSettings.topics.map(topic => Command[Nothing](Subscribe(topic.name))).toList
+  val initialCommands: List[Command[Nothing]] = connectCommand :: subscribeCommands
+
+  val bufferSize: Int = 100
+  val flow
+      : Source[Either[MqttCodec.DecodeError, Event[Nothing]], SourceQueueWithComplete[Nothing]] =
+    Source(initialCommands)
+      .concatMat(Source.queue(bufferSize, OverflowStrategy.backpressure))(Keep.right)
+      .via(sessionFlow)
+      .wireTap(event => logger.debug(s"Received event: $event"))
 }
