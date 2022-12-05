9b757b1a958717956e6b9b2a3bbbbbb42a5058e5
madpeh <48161600+madpeh@users.noreply.github.com>
2022-12-05 12:58:32 +0100

    Add session id

--- src/main/scala/MqttSettings.scala
+++ src/main/scala/MqttSettings.scala
@@ -1,9 +1,14 @@
 package pl.waw.ibspan.scala_mqtt_wrapper
 
+import utils.Uuid.generateUuid
+
+import akka.util.ByteString
+
 final case class MqttSettings(
     username: String,
     password: String,
     host: String,
     port: Int,
-    topics: Seq[MqttTopic]
+    topics: Seq[MqttTopic],
+    sessionId: ByteString = ByteString(generateUuid.toString)
 )
