822ccf9deaee46ed5a180fc9c1b7b799b0ffa379
madpeh <48161600+madpeh@users.noreply.github.com>
2022-12-05 20:59:48 +0100

    Add parallelism to mqtt source parameters

--- src/main/scala/MqttSource.scala
+++ src/main/scala/MqttSource.scala
@@ -8,13 +8,14 @@ import akka.stream.scaladsl.Source
 import akka.util.ByteString
 import com.typesafe.scalalogging.LazyLogging
 
-class MqttSource(mqttSettings: MqttSettings)(implicit system: ActorSystem[_]) extends LazyLogging {
+class MqttSource(mqttSettings: MqttSettings, numParallelFutures: Int = 1)(implicit
+    system: ActorSystem[_]
+) extends LazyLogging {
   val mqttClient: MqttClient = new MqttClient(mqttSettings)
-  val parallelism: Int = 1
   val flow: Source[(ByteString, String), NotUsed] = Source
     .repeat(NotUsed)
     .map(_ => mqttClient.eventQueue.pull())
-    .mapAsync(parallelism)(identity)
+    .mapAsync(numParallelFutures)(identity)
     .wireTap(event => logger.debug(s"Received input event $event"))
     .collect { case Some(Right(Event(p: Publish, _))) =>
       (p.payload, p.topicName)
