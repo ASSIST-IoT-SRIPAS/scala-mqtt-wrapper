a82bff6600ff924d53bef6cbca30107e6808ae38
madpeh <48161600+madpeh@users.noreply.github.com>
2022-12-05 19:28:52 +0100

    Use event queue as source

--- src/main/scala/MqttSource.scala
+++ src/main/scala/MqttSource.scala
@@ -1,16 +1,23 @@
 package pl.waw.ibspan.scala_mqtt_wrapper
 
+import akka.NotUsed
 import akka.actor.typed.ActorSystem
 import akka.stream.alpakka.mqtt.streaming.Event
 import akka.stream.alpakka.mqtt.streaming.Publish
+import akka.stream.scaladsl.Keep
 import akka.stream.scaladsl.Source
 import akka.stream.scaladsl.SourceQueueWithComplete
 import akka.util.ByteString
+import com.typesafe.scalalogging.LazyLogging
 
-class MqttSource(mqttSettings: MqttSettings)(implicit system: ActorSystem[_]) {
+class MqttSource(mqttSettings: MqttSettings)(implicit system: ActorSystem[_]) extends LazyLogging {
   val mqttClient: MqttClient = new MqttClient(mqttSettings)
-  val flow: Source[(ByteString, String), SourceQueueWithComplete[Nothing]] = mqttClient.flow
-    .collect { case Right(Event(p: Publish, _)) =>
+  val flow: Source[(ByteString, String), NotUsed] = Source
+    .repeat(NotUsed)
+    .map(_ => mqttClient.eventQueue.pull())
+    .mapAsync(1)(identity)
+    .wireTap(event => logger.debug(s"Received input event $event"))
+    .collect { case Some(Right(Event(p: Publish, _))) =>
       (p.payload, p.topicName)
     }
 }
