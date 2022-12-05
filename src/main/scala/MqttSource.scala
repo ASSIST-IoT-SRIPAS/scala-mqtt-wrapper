0a0bb9e5ccd78459016506c42e04178d13117af6
madpeh <48161600+madpeh@users.noreply.github.com>
2022-12-05 20:42:32 +0100

    Add parallelism constant

--- src/main/scala/MqttSource.scala
+++ src/main/scala/MqttSource.scala
@@ -4,18 +4,17 @@ import akka.NotUsed
 import akka.actor.typed.ActorSystem
 import akka.stream.alpakka.mqtt.streaming.Event
 import akka.stream.alpakka.mqtt.streaming.Publish
-import akka.stream.scaladsl.Keep
 import akka.stream.scaladsl.Source
-import akka.stream.scaladsl.SourceQueueWithComplete
 import akka.util.ByteString
 import com.typesafe.scalalogging.LazyLogging
 
 class MqttSource(mqttSettings: MqttSettings)(implicit system: ActorSystem[_]) extends LazyLogging {
   val mqttClient: MqttClient = new MqttClient(mqttSettings)
+  val parallelism: Int = 1
   val flow: Source[(ByteString, String), NotUsed] = Source
     .repeat(NotUsed)
     .map(_ => mqttClient.eventQueue.pull())
-    .mapAsync(1)(identity)
+    .mapAsync(parallelism)(identity)
     .wireTap(event => logger.debug(s"Received input event $event"))
     .collect { case Some(Right(Event(p: Publish, _))) =>
       (p.payload, p.topicName)
