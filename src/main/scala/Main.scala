4a4a21524d4d2ab657d8359218d7dd500cef4818
madpeh <48161600+madpeh@users.noreply.github.com>
2022-12-05 15:20:33 +0100

    Add example code

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,5 +3,46 @@ package pl.waw.ibspan.scala_mqtt_wrapper
 object Main {
   def main(args: Array[String]): Unit = {
     println("Scala MQTT wrapper")
+
+    // TODO: remove me
+    // example code
+    import akka.actor.typed.ActorSystem
+    import akka.actor.typed.scaladsl.Behaviors
+    import akka.stream.scaladsl.Flow
+    import akka.util.ByteString
+    import akka.stream.alpakka.mqtt.streaming.ControlPacketFlags
+
+    implicit val system: ActorSystem[Nothing] = ActorSystem[Nothing](
+      Behaviors.setup[Nothing] { context =>
+        context.log.info("system started")
+        Behaviors.empty
+      },
+      name = "ScalaMqttWrapper"
+    )
+    val source = new MqttSource(
+      MqttSettings(
+        host = "mosquitto",
+        port = 1883,
+        topics = Seq(MqttTopic("input"))
+      )
+    )
+    val sink = new MqttSink(
+      MqttSettings(
+        host = "mosquitto",
+        port = 1883
+      )
+    )
+    source.flow
+      .via(Flow[(ByteString, String)].map { case (msg, topic) =>
+        val outputMsg = ByteString(msg.utf8String.toUpperCase)
+        val outputTopic = "output"
+        val publishFlags = ControlPacketFlags.QoSAtLeastOnceDelivery | ControlPacketFlags.RETAIN
+        println(
+          s"source [$topic] ${msg.utf8String} --> sink [$outputTopic] ${outputMsg.utf8String}"
+        )
+        (outputMsg, outputTopic, publishFlags)
+      })
+      .to(sink.flow)
+      .run()
   }
 }
