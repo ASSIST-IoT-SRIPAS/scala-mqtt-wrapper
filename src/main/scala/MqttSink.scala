f85ed62ef3b57a9de401a0aab4cec26b2afe9a15
madpeh <48161600+madpeh@users.noreply.github.com>
2022-12-06 01:19:34 +0100

    Add kill switches

--- src/main/scala/MqttSink.scala
+++ src/main/scala/MqttSink.scala
@@ -1,25 +1,28 @@
 package pl.waw.ibspan.scala_mqtt_wrapper
 
-import akka.NotUsed
 import akka.actor.typed.ActorSystem
+import akka.stream.KillSwitches
+import akka.stream.UniqueKillSwitch
 import akka.stream.alpakka.mqtt.streaming.Command
 import akka.stream.alpakka.mqtt.streaming.ControlPacketFlags
 import akka.stream.alpakka.mqtt.streaming.Publish
 import akka.stream.scaladsl.Flow
+import akka.stream.scaladsl.Keep
 import akka.stream.scaladsl.Sink
 import akka.util.ByteString
 import com.typesafe.scalalogging.LazyLogging
 
 class MqttSink(mqttSettings: MqttSettings)(implicit system: ActorSystem[_]) extends LazyLogging {
   val mqttClient: MqttClient = new MqttClient(mqttSettings)
-  mqttClient.source.runWith(Sink.ignore)
-  val flow: Sink[(ByteString, String, ControlPacketFlags), NotUsed] =
+  val flow: Sink[(ByteString, String, ControlPacketFlags), UniqueKillSwitch] =
     Flow[(ByteString, String, ControlPacketFlags)]
       .wireTap(data =>
         logger.debug(s"Sending message [${data._1.utf8String}] to topic [${data._2}]")
       )
-      .map { case (msg, topic, publishFlags) =>
+      .collect { case (msg, topic, publishFlags) =>
         mqttClient.session ! Command[Nothing](Publish(publishFlags, topic, msg))
       }
+      .concatMat(mqttClient.source)(Keep.right)
+      .viaMat(KillSwitches.single)(Keep.right)
       .to(Sink.ignore)
 }
--- src/main/scala/MqttSource.scala
+++ src/main/scala/MqttSource.scala
@@ -1,16 +1,19 @@
 package pl.waw.ibspan.scala_mqtt_wrapper
 
-import akka.NotUsed
 import akka.actor.typed.ActorSystem
+import akka.stream.KillSwitches
+import akka.stream.UniqueKillSwitch
 import akka.stream.alpakka.mqtt.streaming.Event
 import akka.stream.alpakka.mqtt.streaming.Publish
+import akka.stream.scaladsl.Keep
 import akka.stream.scaladsl.Source
 import akka.util.ByteString
 import com.typesafe.scalalogging.LazyLogging
 
 class MqttSource(mqttSettings: MqttSettings)(implicit system: ActorSystem[_]) extends LazyLogging {
   val mqttClient: MqttClient = new MqttClient(mqttSettings)
-  val flow: Source[(ByteString, String), NotUsed] = mqttClient.source
+  val flow: Source[(ByteString, String), UniqueKillSwitch] = mqttClient.source
+    .viaMat(KillSwitches.single)(Keep.right)
     .collect { case Right(Event(p: Publish, _)) =>
       (p.payload, p.topicName)
     }
