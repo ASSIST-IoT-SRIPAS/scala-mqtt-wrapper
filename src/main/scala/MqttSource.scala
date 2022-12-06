a8a1ad93908891b7394fe2a26dfe4a5c939a4b5b
madpeh <48161600+madpeh@users.noreply.github.com>
2022-12-06 16:48:10 +0100

    Change MqttSource to object

--- src/main/scala/MqttSource.scala
+++ src/main/scala/MqttSource.scala
@@ -8,8 +8,10 @@ import akka.stream.scaladsl.Source
 import akka.util.ByteString
 import com.typesafe.scalalogging.LazyLogging
 
-class MqttSource(mqttClient: MqttClient)(implicit system: ActorSystem[_]) extends LazyLogging {
-  val flow: Source[(ByteString, String), NotUsed] = mqttClient.sourceBroadcast
+object MqttSource extends LazyLogging {
+  def source(mqttClient: MqttClient)(implicit
+      system: ActorSystem[_]
+  ): Source[(ByteString, String), NotUsed] = mqttClient.sourceBroadcast
     .collect { case Right(Event(p: Publish, _)) =>
       (p.payload, p.topicName)
     }
