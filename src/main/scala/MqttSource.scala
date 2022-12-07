e499c563c757fe60b148eea80dc71f2343bddc8d
madpeh <48161600+madpeh@users.noreply.github.com>
2022-12-07 03:48:43 +0100

    Remove unused imports

--- src/main/scala/MqttSource.scala
+++ src/main/scala/MqttSource.scala
@@ -2,8 +2,6 @@ package pl.waw.ibspan.scala_mqtt_wrapper
 
 import akka.NotUsed
 import akka.actor.typed.ActorSystem
-import akka.stream.alpakka.mqtt.streaming.Event
-import akka.stream.alpakka.mqtt.streaming.Publish
 import akka.stream.scaladsl.Source
 import akka.util.ByteString
 import com.typesafe.scalalogging.LazyLogging
