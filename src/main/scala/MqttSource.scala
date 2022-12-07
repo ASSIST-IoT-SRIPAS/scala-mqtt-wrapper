ad57a53c59f7880e9f15befe0389d630b8d4099b
madpeh <48161600+madpeh@users.noreply.github.com>
2022-12-07 15:42:44 +0100

    Remove unused implicit actor system from source

--- src/main/scala/MqttSource.scala
+++ src/main/scala/MqttSource.scala
@@ -1,7 +1,6 @@
 package pl.waw.ibspan.scala_mqtt_wrapper
 
 import akka.NotUsed
-import akka.actor.typed.ActorSystem
 import akka.stream.scaladsl.Source
 import akka.util.ByteString
 import com.typesafe.scalalogging.LazyLogging
@@ -14,16 +13,13 @@ object MqttSource extends LazyLogging {
     *
     * @param mqttClient
     *   MQTT client
-    * @param system
-    *   actor system
     */
-  def source(mqttClient: MqttClient)(implicit
-      system: ActorSystem[_]
-  ): Source[(ByteString, String), NotUsed] = mqttClient.publishEventBroadcast
-    .wireTap(data =>
-      logger.debug(
-        "[%s] Received message [%s] from topic [%s]"
-          .format(mqttClient.name, data._1.utf8String, data._2)
+  def source(mqttClient: MqttClient): Source[(ByteString, String), NotUsed] =
+    mqttClient.publishEventBroadcast
+      .wireTap(data =>
+        logger.debug(
+          "[%s] Received message [%s] from topic [%s]"
+            .format(mqttClient.name, data._1.utf8String, data._2)
+        )
       )
-    )
 }
