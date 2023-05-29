b631c759c6711cabcbd52da9376980462cf14a58
madpeh <48161600+madpeh@users.noreply.github.com>
2023-05-29 18:26:46 +0200

    Add flags and packet id to received message

--- src/main/scala/MqttReceivedMessage.scala
+++ src/main/scala/MqttReceivedMessage.scala
@@ -1,5 +1,7 @@
 package pl.waw.ibspan.scala_mqtt_wrapper
 
+import akka.stream.alpakka.mqtt.streaming.ControlPacketFlags
+import akka.stream.alpakka.mqtt.streaming.PacketId
 import akka.util.ByteString
 
 /** data received from the MQTT broker
@@ -7,8 +9,14 @@ import akka.util.ByteString
   *   received payload
   * @param topic
   *   topic from which the payload was received
+  * @param flags
+  *   MQTT flags
+  * @param packetId
+  *   MQTT packet id
   */
 final case class MqttReceivedMessage(
     payload: ByteString,
     topic: String,
+    flags: ControlPacketFlags,
+    packetId: Option[PacketId],
 )
