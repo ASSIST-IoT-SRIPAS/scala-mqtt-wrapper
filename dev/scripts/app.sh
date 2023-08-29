95de5d30b7a4f63b5b1f8237edcb2a3556561a6e
Przemyslaw Holda <48161600+madpeh@users.noreply.github.com>
2023-08-29 23:08:48 +0000

    Update dev environment (#26)
    * Change docker-compose to docker compose
    
    * Fix and simplify app dockerfile
    
    * Bump mosquitto version
    
    * Update mosquitto conf
    
    * Add vernemq
    
    * Update compose file

--- dev/scripts/app.sh
+++ dev/scripts/app.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker-compose -f dev/compose/docker-compose.yml up app --build --no-log-prefix
+docker compose -f dev/compose/docker-compose.yml up app --build --no-log-prefix
--- dev/scripts/env.sh
+++ dev/scripts/env.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker-compose -f dev/compose/docker-compose.yml up mosquitto mqtt-explorer --build
+docker compose -f dev/compose/docker-compose.yml up mosquitto mqtt-explorer vernemq --build
