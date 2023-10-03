32d5cea18e56b57027206aabcc60d5d7f5b1d996
madpeh <48161600+madpeh@users.noreply.github.com>
2023-10-03 23:04:25 +0200

    Add akka code

---
+++ modules/akka/src/main/scala/MqttPublishMessage.scala
@@ -0,0 +1,19 @@
+package pl.waw.ibspan.scala_mqtt_wrapper.akka
+
+import akka.stream.alpakka.mqtt.streaming.ControlPacketFlags
+import akka.util.ByteString
+
+/** MQTT data to be published
+  *
+  * @param payload
+  *   payload to be published
+  * @param topic
+  *   topic to which the payload will be published
+  * @param flags
+  *   MQTT flags
+  */
+final case class MqttPublishMessage(
+    payload: ByteString,
+    topic: String,
+    flags: ControlPacketFlags = PublishQoSFlags.QoSAtMostOnceDelivery,
+)
---
+++ modules/akka/src/main/scala/MqttSource.scala
@@ -0,0 +1,32 @@
+package pl.waw.ibspan.scala_mqtt_wrapper.akka
+
+import akka.NotUsed
+import akka.stream.scaladsl.Source
+
+object MqttSource {
+
+  /** Create MQTT source
+    *
+    * Source consumes messages from the MQTT broker from subscribed topics
+    *
+    * @param mqttClient
+    *   MQTT client
+    * @param loggingSettings
+    *   optional logging settings
+    */
+  def source(
+      mqttClient: MqttClient,
+      loggingSettings: Option[MqttLoggingSettings] = None,
+  ): Source[MqttReceivedMessage, NotUsed] = {
+    loggingSettings.fold(mqttClient.publishEventBroadcastSource) { settings =>
+      val name = s"${mqttClient.name} : ${settings.name}"
+      mqttClient.publishEventBroadcastSource
+        .log(
+          name,
+          data =>
+            s"payload [${data.payload.utf8String}] from topic [${data.topic}] with flags [${data.flags}] and packet id [${data.packetId}]",
+        )
+        .addAttributes(settings.attributes)
+    }
+  }
+}
